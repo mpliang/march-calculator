86036da10ab769b3e80b1b3b5f5f9e07ecc17bed
OT-mliang <mliang@opentable.com>
2021-06-03 10:55:12 -0500

    Initial Commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,9 +12,9 @@ export default () => {
     <>
       <Helmet>
         <meta charSet="utf-8" />
-        <title>{title || 'Gatsby Simplefolio'}</title>
+        <title>{'March Calculator by Loki'}</title>
         <html lang={lang || 'en'} />
-        <meta name="description" content={description || 'Gatsby Simplefolio'} />
+        <meta name="March Calculator by Loki" />
       </Helmet>
       <App />
     </>
