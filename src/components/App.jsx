1aa9cea60bc99c6728798b533e6147dce11c48ad
OT-mliang <mliang@opentable.com>
2021-06-03 11:18:40 -0500

    Update css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,21 +8,21 @@ import Calculator from './Calculator/Calculator';
 function App() {
   return (
     <div>
-      <section>
+      <section id="hero">
         <Container>
           <Fade duration={1000} delay={500} distance="30px">
             <h1 className="hero-title">
-            <span className="text-color-main">March Time Calculator</span>
-          </h1>
-        </Fade>
-        <Fade duration={1000} delay={1000} distance="30px"></Fade>
-      </Container>
+              <span className="text-color-main">March Time Calculator</span>
+            </h1>
+          </Fade>
+          <Fade duration={1000} delay={1000} distance="30px"></Fade>
+        </Container>
       </section>
       <div className='body'>
         <Calculator />
         <Clock />
       </div>
-      <div className='footer'>Dedicated to those that struggle with math: MrH and CDBGM</div>
+      <div className='footer'>For those that struggle with math (MrH and CDBGM)</div>
     </div>
   )
 }
